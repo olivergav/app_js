5049e221f7946d3727aa17e1138ba4e884dd7d13
olivergav <oliwier.gawlik0@gmail.com>
2022-06-23 19:24:49 +0200

    Magic with foReach, map

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,13 +1,19 @@
-function magic(texts, userName, userAge){
-    console.log(texts)
-    return  `${userName}, ${userAge}`;
+function magic(texts, ...vars){
+    vars[0] = vars[0][0].toUpperCase() + vars[0].slice(1);
+    // let text = '';
+
+    // texts.forEach((t, id) => {
+    //     text += t + (vars[id] ? vars[id] : '');
+    // })
+    return  texts
+        .map((t, id) => t + (vars[id] ? vars[id] : '')).join('')
 }
 
 
 const name = 'pawel';
 const age = 37;
-
+const animal = 'wszy'
 //tagged template literal string
-const sentence = magic`${name}${age}`;
+const sentence = magic`Mam na imię ${name} i mam ${age} lat. Ala ma kota i ma ${animal}.`;
 
-console.log(sentence)
+console.log(sentence);
